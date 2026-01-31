40d56e9688f86b78869ad3e777f4262831a92603
kanha-beep <kanhashree2223@gmail.com>
2026-02-01 04:09:50 +0530

    msg

--- routes/brand.Routes.js
+++ routes/brand.Routes.js
@@ -3,8 +3,16 @@ import { brand } from "../models/Models.js"
 const router = express.Router()
 const allBrand = async (req, res) => {
     const brands = await brand.find({})
-    console.log("brands: ", brands)
+    // console.log("brands: ", brands)
     res.json({ brands, message: "Hello from contact controller" })
 }
+const singleBrand = async (req, res) => {
+    const { brandId } = req.params;
+    console.log("got brand Id: ", brandId);
+    const singleBrand = await brand.findById(brandId)
+    console.log("single brand: ", singleBrand)
+    res.json({ singleBrand, message: "Hello from contact controller" })
+}
 router.get("/", allBrand)
+router.get("/:brandId", singleBrand)
 export default router
