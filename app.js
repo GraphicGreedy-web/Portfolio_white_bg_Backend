a1f1a93401f80eae4c813cb8ab4b86337934427b
GraphicGreedy-web <shree.gupta2317@gmail.com>
2026-02-01 04:33:53 +0530

    Update allowedOrigin to use FRONTEND_URL

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import visualCommRoutes from "./routes/visualComm.Routes.js"
 import { connectDB } from "./db.js"
 const app = express()
 // const allowedOrigin = process.env.FRONTEND_URL
-const allowedOrigin = process.env.FRONTEND_URI.split(",").map(o => o.trim())
+const allowedOrigin = process.env.FRONTEND_URL.split(",").map(o => o.trim())
 console.log("back: ", allowedOrigin)
 app.use(cors({
     origin: allowedOrigin,
